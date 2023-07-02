5f2d77337759c148139a9ef3ed4fdfdbdb4a5318
Christopher Fujino <christopherfujino@gmail.com>
2023-07-01 22:06:26 -0700

    implement months

--- data_engine/lib/src/calendar.dart
+++ data_engine/lib/src/calendar.dart
@@ -8,27 +8,37 @@ class Moment {
   final Year year;
   final Month month;
 
-  Moment operator +(final Object other) {
-    if (other is Year) {
-      return Moment(
-        month: month,
-        year: Year(year.value + other.value),
-      );
+  Moment operator +(final TimePart other) {
+    switch (other) {
+      case Year():
+        return Moment(month: month, year: Year(year.value + other.value));
+      case Month():
+        final int nextMonthValue = month.value + other.value;
+        if (nextMonthValue >= 12) {
+          return Moment(
+            month: Month(nextMonthValue % 12),
+            year: Year(year.value + 1),
+          );
+        }
+        return Moment(
+          month: Month(nextMonthValue),
+          year: year,
+        );
+      default:
+        throw UnimplementedError('TODO');
     }
-    if (other is Month) {
-      throw UnimplementedError('TODO');
-    }
-    throw UnimplementedError('Foo bar');
   }
 }
 
-class Year {
+sealed class TimePart {}
+
+class Year implements TimePart {
   const Year(this.value) : assert(value > 0);
 
   final int value;
 }
 
-class Month {
+class Month implements TimePart {
   const Month(this.value) : assert(value < 12 && value >= 0);
 
   final int value;
