d03f0d25d96d62ade9aeff65bf983f1999ab8d79
Christopher Fujino <christopherfujino@gmail.com>
2023-07-03 19:14:53 -0700

    fix analysis

--- data_engine/bin/test.dart
+++ data_engine/bin/test.dart
@@ -4,7 +4,5 @@ Future<void> main() async => run(Context.real);
 
 Future<void> run(final Context ctx) async {
   final List<Asset> assets = Asset.defaults;
-  for (final Asset asset in assets) {
-    ctx.print(asset);
-  }
+  assets.forEach(ctx.print);
 }
--- data_engine/lib/foo.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-}
-
-class Klass {
-  Klass(this._foo);
-  void Function(int bar) foo = (bool bar) => _foo(bar);
-  final void Function(int bar) _foo;
-}
